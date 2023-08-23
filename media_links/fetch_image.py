d532c2729bf21ad3e35eea63d30b11a2e312164f
Adelard Dcunha <83525584+adlard07@users.noreply.github.com>
2023-08-23 12:44:03 +0530

    Update fetch_image.py

--- media_links/fetch_image.py
+++ media_links/fetch_image.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 from dataclasses import dataclass
 
 def fetch_image_urls(query: str, max_links_to_fetch: int, wd: webdriver, sleep_between_interactions: int = 2):
-    search_url = "https://www.google.com/search?sca_esv=557132508&rlz=1C1VDKB_enIN970IN970&q=coding+memes&tbm=isch&source=lnms&sa=X&ved=2ahUKEwiUu6_Ght-AAxVVGogKHa97B98Q0pQJegQICxAB&biw=1366&bih=619&dpr=1"
+    search_url = "https://www.google.com/search?sca_esv=557132508&rlz=1C1VDKB_enIN970IN970&q={search_term.replace(' ', +)}&tbm=isch&source=lnms&sa=X&ved=2ahUKEwiUu6_Ght-AAxVVGogKHa97B98Q0pQJegQICxAB&biw=1366&bih=619&dpr=1"
     wd.get(search_url.format(q=query))
     image_urls= set()
     imageCount = 0
@@ -40,7 +40,7 @@ class GetItems:
     def get_image(self):
         wd = webdriver.Chrome()
         search_term = "coding memes"
-        num_images = 100
+        num_images = 1000
         image_url = fetch_image_urls(search_term, num_images, wd)
 
         return image_url
